673586b3cc1a36fad188ac1d7afd0ee690708f81
Hack Reactor Students <dchk90@gmail.com>
2014-04-30 15:41:28 -0700

    adds enqueue and specifies that it is a collection of SongModels

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,14 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
+  model: SongModel,
+
   initialize: function(){
+
+  },
+
+  enqueue: function(song){
+    this.push(song);
   }
 
 });
